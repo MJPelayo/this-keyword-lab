7a0f755d2a7dc4b93781fc2df0ba2cc222e67775
Marlon Pelayo <2021153565@ub.edu.bz>
2026-02-14 21:44:49 -0600

    Complete Exercise 1: Multi-Button Highlight

---
+++ exercise1.js
@@ -0,0 +1,10 @@
+const buttons = document.querySelectorAll(".color-btn");
+
+function changeColor() {
+  this.style.backgroundColor = "yellow";
+}
+
+buttons.forEach(function(button) {
+  button.addEventListener("click", changeColor);
+});
+
